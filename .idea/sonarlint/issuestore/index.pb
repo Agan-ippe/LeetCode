fb64a006542b1072f4180438a89d35149221b941
Agan-ippe <wjy7136@163.com>
2024-03-01 17:29:35 +0800

    2024.03.01完成了leetcode第一题，还待优化。

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+]
+-src/com/leetcode/_01Array_and_String/_01.java,2\d\2d643c34a2cad99300382a316ca8a621c8314d50
---
+++ src/com/leetcode/_001st_Week/TwoSum.java
@@ -0,0 +1,29 @@
+package com.leetcode._001st_Week;
+
+import java.util.Arrays;
+
+/**
+ * @author Aip
+ * @version 1.0
+ * @date 2024/3/1  16:02
+ * @description 两数之和,这里用到的方法是暴力枚举，直接使用两个for来遍历。这样做的时间复杂度较大。优化方式：可以使用哈希表。目前并没有领略到精髓
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(nums,9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] +nums[j]){
+                    int[] count={i,j};
+                    return count;
+                }
+            }
+        }
+        return null;
+    }
+
+}
